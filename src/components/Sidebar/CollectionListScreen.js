4a2e3423e945e4450e56ae6ac3bcc62ce6405e37
Ian Logan <itlogan9@gmail.com>
2020-05-03 09:10:05 -0500

    Refactor form styling into seperate directory

--- src/components/Sidebar/CollectionListScreen.js
+++ src/components/Sidebar/CollectionListScreen.js
@@ -1,24 +1,9 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import styled from '@emotion/styled';
 
 import { useState } from 'react';
 import { useCollections } from '../../context/collectionsContext';
-const FormContainer = styled.form({
-  padding: '.75rem',
-  backgroundColor: '#FFFFFF',
-  boxShadow: 'inset 0px 1px 10px rgba(0,0,0,20%)',
-  color: 'black',
-});
-const FormGroup = styled.div({
-  display: 'flex',
-  flexDirection: 'column',
-  marginBottom: '1rem',
-});
-
-const Row = styled.div({
-  display: 'flex',
-});
+import { FormContainer, FormGroup, Row } from './Forms';
 
 function CollectionListScreen() {
   const { collections, setCollections } = useCollections();
---
+++ src/components/Sidebar/Forms.js
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+
+export const FormContainer = styled.form({
+  padding: '.75rem',
+  backgroundColor: '#FFFFFF',
+  boxShadow: 'inset 0px 1px 10px rgba(0,0,0,20%)',
+  color: 'black',
+});
+export const FormGroup = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  marginBottom: '1rem',
+});
+
+export const Row = styled.div({
+  display: 'flex',
+  flexWrap: 'wrap',
+});
