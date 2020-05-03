790be41baa950be3b33cc9f029c9033706bfb8ae
Ian Logan <itlogan9@gmail.com>
2020-05-03 12:22:16 -0500

    Update sidebar ui in tree-screen when selecting tiers

--- src/components/Sidebar/TreeScreen.js
+++ src/components/Sidebar/TreeScreen.js
@@ -1,5 +1,6 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
+import styled from '@emotion/styled';
 
 import { useState, Fragment } from 'react';
 import { useCollections } from '../../context/collectionsContext';
@@ -13,19 +14,27 @@ import {
 function HomeScreen() {
   // global state
   const { collection, setCollection } = useCollections();
-  const { nodeType, wrappers } = collection;
+  const { nodeType, wrappers, tierType } = collection;
 
   // local state
   const [nodeName, setNodeName] = useState('');
+  const selectedWrapperTemplate = {
+    name: 'n/a',
+    tier: null,
+    fields: [],
+  };
+  const [selectedWrapper, setSelectedWrapper] = useState(
+    selectedWrapperTemplate
+  );
   const [fields, setFields] = useState([]);
   const fieldTemplate = { label: '', value: '' };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const updatedWrappers = [
-      ...collection.wrappers,
+      ...collection.selectedW,
       {
-        id: collection.wrappers.length + 1,
+        id: collection.selectedW.length + 1,
         name: nodeName,
         fields,
       },
@@ -35,6 +44,14 @@ function HomeScreen() {
     setFields([]);
   };
 
+  // wrappers
+  function handleSelectedWrapperChange(e) {
+    const currWrapper = wrappers.find((w) => w.name === e.target.value);
+    if (!currWrapper) return setSelectedWrapper(selectedWrapperTemplate);
+    setSelectedWrapper(currWrapper);
+  }
+
+  // fields
   function addField() {
     setFields([...fields, { ...fieldTemplate }]);
   }
@@ -49,13 +66,23 @@ function HomeScreen() {
   return (
     <div>
       <label htmlFor="wrapper">Wrapper:</label>
-      <select id="wrapper">
-        <option>wrapper 1</option>
-        <option>wrapper 2</option>
-        {wrappers ? console.log(wrappers) : null}
+      <select
+        id="wrapper"
+        value={selectedWrapper.name}
+        onChange={handleSelectedWrapperChange}
+      >
+        <option value={'n/a'}>select wrapper</option>
+        {wrappers
+          ? wrappers.map((w) => (
+              <option key={w.id} value={w.name}>
+                {w.name}
+              </option>
+            ))
+          : ''}
       </select>
       <h3>Add {nodeType}</h3>
       <FormContainer onSubmit={handleSubmit}>
+        <WrapperPresets tierType={tierType} wrapper={selectedWrapper} />
         <FormGroup>
           <label htmlFor="nodeType">{nodeType} name:</label>
           <input
@@ -106,6 +133,51 @@ function HomeScreen() {
         </FormGroup>
       </FormContainer>
     </div>
+  );
+}
+
+const WrapperPresetsContainer = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  textAlign: 'center',
+  fontSize: '.75rem',
+  marginBottom: '1rem',
+});
+
+const WrapperPresetsHeading = styled.div({
+  margin: '0 auto',
+  borderBottom: '1px solid black',
+});
+
+const WrapperPresetContent = styled.div({
+  fontStyle: 'italic',
+});
+
+function WrapperPresets({ tierType, wrapper }) {
+  console.log(wrapper);
+  return (
+    <WrapperPresetsContainer>
+      <WrapperPresetsHeading>Wrapper Presets</WrapperPresetsHeading>
+      <WrapperPresetContent>
+        {wrapper.tier === null ? (
+          <div>none</div>
+        ) : (
+          <Fragment>
+            <div>
+              {tierType}: {wrapper.tier}
+            </div>
+            {wrapper.fields.map((f, i) => {
+              const fieldId = `field-${i}`;
+              return (
+                <div key={fieldId}>
+                  <span>{f.label}</span>: {f.value}
+                </div>
+              );
+            })}
+          </Fragment>
+        )}
+      </WrapperPresetContent>
+    </WrapperPresetsContainer>
   );
 }
 
